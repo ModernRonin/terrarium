01996c807b38ec3d1a84bb3816dc7a209150551a
max <7413294+ModernRonin@users.noreply.github.com>
2017-11-15 22:40:05 +0100

    extracted Renderer

---
+++ Terrarium/prototypes/SimulationView/Renderer.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media;
+using SimulationView.Model;
+
+namespace SimulationView {
+    public class Renderer
+    {
+        public Simulation Simulation { get; set; } = new Simulation();
+        public DrawingGroup Root { get; }= new DrawingGroup();
+        public void Render()
+        {
+            
+        }
+    }
+}
---
+++ Terrarium/prototypes/SimulationView/SimulationViewer.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Media;
+
+namespace SimulationView
+{
+    public partial class SimulationViewer
+    {
+        
+        public SimulationViewer()
+        {
+            InitializeComponent();
+        }
+        public Renderer Renderer { get; set; }= new Renderer();
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            Renderer.Render();
+            drawingContext.DrawDrawing(Renderer.Root);
+        }
+    }
+}
