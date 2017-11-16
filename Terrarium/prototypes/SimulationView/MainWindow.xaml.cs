d49ca1fa200159b8357324d1309e38ef6378e5c3
max <7413294+ModernRonin@users.noreply.github.com>
2017-11-16 03:54:23 +0100

    added start/stop buttons and corresponding calls on simulation

--- Terrarium/prototypes/SimulationView/MainWindow.xaml.cs
+++ Terrarium/prototypes/SimulationView/MainWindow.xaml.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using SimulationView.Model;
 
 namespace SimulationView
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        readonly Simulation mSimulation = new Simulation(SimulationState.Default);
         public MainWindow()
         {
             InitializeComponent();
+            StartButton.IsEnabled = true;
+            StopButton.IsEnabled = false;
+        }
+        void OnStart(object sender, RoutedEventArgs e)
+        {
+            StartButton.IsEnabled = false;
+            mSimulation.Start();
+            StopButton.IsEnabled = true;
+        }
+        async void OnStop(object sender, RoutedEventArgs e)
+        {
+            StopButton.IsEnabled = false;
+            await mSimulation.Stop();
+            StartButton.IsEnabled = true;
         }
     }
 }
