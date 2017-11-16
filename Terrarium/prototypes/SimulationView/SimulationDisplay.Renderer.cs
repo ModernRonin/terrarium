0b2e137b203fb2eb4b7c2cf8f4b0d38423fcdc9e
max <7413294+ModernRonin@users.noreply.github.com>
2017-11-16 02:10:42 +0100

    extracted Renderer

---
+++ Terrarium/prototypes/SimulationView/SimulationDisplay.Renderer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using ModernRonin.PraeterArtem.Functional;
+using SimulationView.Model;
+
+namespace SimulationView
+{
+    public class Renderer : IDisposable
+    {
+        readonly DrawingContext mContext;
+        readonly Size mRenderSize;
+        readonly Vector mScalingFactor;
+        readonly Simulation mSimulation;
+        public Renderer(DrawingGroup drawingGroup, Size renderSize, Simulation simulation)
+        {
+            mContext = drawingGroup.Open();
+            mRenderSize = renderSize;
+            mSimulation = simulation;
+            mScalingFactor = new Vector(mRenderSize.Width / mSimulation.Size.Width,
+                mRenderSize.Height / mSimulation.Size.Height);
+        }
+        public void Dispose()
+        {
+            mContext.Close();
+        }
+        public void Render()
+        {
+            mContext.DrawRectangle(Brushes.Black, null, new Rect(mRenderSize));
+            mSimulation.Entities.UseIn(Draw);
+        }
+        void Draw(Entity entity)
+        {
+            void drawRectangle(FilledRectangle rectangle)
+            {
+                mContext.DrawRectangle(rectangle.Brush, null, rectangle.Rectangle);
+            }
+
+            entity.Parts.Select(p => ToRectangle(p, entity.Position)).UseIn(drawRectangle);
+        }
+        FilledRectangle ToRectangle(Part part, Vector origin)
+        {
+            var position = (origin + part.RelativePosition).ScaleBy(mScalingFactor);
+            return new FilledRectangle
+            {
+                Brush = ToColor(part.Kind),
+                Rectangle = new Rect((Point) position, (Size) mScalingFactor)
+            };
+        }
+        static Brush ToColor(PartKind kind)
+        {
+            switch (kind)
+            {
+                case PartKind.Core:
+                    return Brushes.Aquamarine;
+
+                case PartKind.Absorber:
+                    return Brushes.LightGreen;
+                case PartKind.Store:
+                    return Brushes.BurlyWood;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(kind), kind, null);
+            }
+        }
+
+        class FilledRectangle
+        {
+            public Rect Rectangle { get; set; }
+            public Brush Brush { get; set; }
+        }
+    }
+}
--- Terrarium/prototypes/SimulationView/SimulationDisplay.cs
+++ Terrarium/prototypes/SimulationView/SimulationDisplay.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Media;
-using ModernRonin.PraeterArtem.Functional;
 using SimulationView.Model;
 
 namespace SimulationView
@@ -24,55 +21,7 @@ namespace SimulationView
         }
         void Render()
         {
-            var context = mBackPage.Open();
-            Render(context);
-            context.Close();
-        }
-        void Render(DrawingContext context)
-        {
-            context.DrawRectangle(Brushes.Black, null, new Rect(RenderSize));
-            Simulation.Entities.UseIn(e => Draw(context, e));
-        }
-        void Draw(DrawingContext context, Entity entity)
-        {
-            void drawRectangle(FilledRectangle rectangle)
-            {
-                context.DrawRectangle(rectangle.Brush, null, rectangle.Rectangle);
-            }
-
-            entity.Parts.Select(p => ToRectangle(p, entity.Position)).UseIn(drawRectangle);
-        }
-        FilledRectangle ToRectangle(Part part, Vector origin)
-        {
-            var factor = new Vector(RenderSize.Width / Simulation.Size.Width,
-                RenderSize.Height / Simulation.Size.Height);
-            var position = (origin + part.RelativePosition).ScaleBy(factor);
-            return new FilledRectangle
-            {
-                Brush = ToColor(part.Kind),
-                Rectangle = new Rect((Point) position, (Size) factor)
-            };
-        }
-        static Brush ToColor(PartKind kind)
-        {
-            switch (kind)
-            {
-                case PartKind.Core:
-                    return Brushes.Aquamarine;
-
-                case PartKind.Absorber:
-                    return Brushes.LightGreen;
-                case PartKind.Store:
-                    return Brushes.BurlyWood;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(kind), kind, null);
-            }
-        }
-
-        class FilledRectangle
-        {
-            public Rect Rectangle { get; set; }
-            public Brush Brush { get; set; }
+            using (var renderer = new Renderer(mBackPage, RenderSize, Simulation)) { renderer.Render(); }
         }
     }
 }
