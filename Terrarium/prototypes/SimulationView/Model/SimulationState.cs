2f7c81406fb7bbb1eef511e80ae9c12d23588698
max <7413294+ModernRonin@users.noreply.github.com>
2017-11-16 03:40:07 +0100

    default ctor now creates empty simulation, moved setup to factory property instead

--- Terrarium/prototypes/SimulationView/Model/SimulationState.cs
+++ Terrarium/prototypes/SimulationView/Model/SimulationState.cs
@@ -5,8 +5,12 @@ namespace SimulationView.Model
 {
     public class SimulationState
     {
-        public Size Size { get; set; } = new Size(100, 100);
-        public IEnumerable<Entity> Entities { get; set; } =
-            new List<Entity> {Entity.Cross.At(new Vector(10, 10)), Entity.Snake.At(new Vector(90, 90))};
+        public Size Size { get; set; }
+        public IEnumerable<Entity> Entities { get; set; }
+        public static SimulationState Default => new SimulationState
+        {
+            Size = new Size(100, 100),
+            Entities = new List<Entity> {Entity.Cross.At(new Vector(10, 10)), Entity.Snake.At(new Vector(90, 90))}
+        };
     }
 }
