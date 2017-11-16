f6c629656934117f6979956ba7e9f339e4dbba18
max <7413294+ModernRonin@users.noreply.github.com>
2017-11-16 03:37:47 +0100

    added some simplistic movement, just so we can test the view update

--- Terrarium/prototypes/SimulationView/Model/SimulationState.cs
+++ Terrarium/prototypes/SimulationView/Model/SimulationState.cs
@@ -5,8 +5,8 @@ namespace SimulationView.Model
 {
     public class SimulationState
     {
-        public Size Size { get; } = new Size(100, 100);
-        public IList<Entity> Entities { get; } =
+        public Size Size { get; set; } = new Size(100, 100);
+        public IEnumerable<Entity> Entities { get; set; } =
             new List<Entity> {Entity.Cross.At(new Vector(10, 10)), Entity.Snake.At(new Vector(90, 90))};
     }
 }
--- Terrarium/prototypes/SimulationView/Model/SimulationTicker.cs
+++ Terrarium/prototypes/SimulationView/Model/SimulationTicker.cs
@@ -1,10 +1,32 @@
-﻿namespace SimulationView.Model
+﻿using System.Linq;
+using System.Windows;
+using ModernRonin.PraeterArtem.Functional;
+
+namespace SimulationView.Model
 {
     public class SimulationTicker
     {
         readonly SimulationState mCurrent;
+        readonly Vector[] mDirections = {new Vector(1, 1), new Vector(-3, -7)};
         readonly SimulationState mNext = new SimulationState();
-        public SimulationTicker(SimulationState current) => mCurrent = current;
-        public SimulationState Tick() => mNext;
+        int mDirectionIndex = 0;
+        public SimulationTicker(SimulationState current)
+        {
+            mCurrent = current;
+            mNext.Size = mCurrent.Size;
+            mDirections.UseIn(v => v.Normalize());
+        }
+        public SimulationState Tick()
+        {
+            mNext.Entities = mCurrent.Entities.Select(Move).ToArray();
+            return mNext;
+        }
+        Entity Move(Entity old)
+        {
+            mDirectionIndex = 0 == mDirectionIndex ? 1 : 0;
+            var newPosition = old.Position + mDirections[mDirectionIndex];
+            // TODO: make Simulation, Entity etc. all immutable so we can't forget to treat them as such
+            return new Entity {Parts = old.Parts, Position = newPosition};
+        }
     }
 }
