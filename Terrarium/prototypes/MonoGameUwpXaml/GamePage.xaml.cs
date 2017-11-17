9722bf644ae1337f2a4c502cbb600261670213b0
max <7413294+ModernRonin@users.noreply.github.com>
2017-11-17 15:57:56 +0100

    Monogame running in a panel

---
+++ Terrarium/prototypes/MonoGameUwpXaml/GamePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Windows.UI.Xaml;
+using MonoGame.Framework;
+
+namespace MonoGameUwpXaml
+{
+    public sealed partial class GamePage
+    {
+        readonly Game1 _game;
+        public GamePage()
+        {
+            InitializeComponent();
+
+            // Create the game.
+            var launchArguments = string.Empty;
+            _game = XamlGame<Game1>.Create(launchArguments, Window.Current.CoreWindow, swapChainPanel);
+        }
+        void OnStart(object sender, RoutedEventArgs e) { }
+        void OnStop(object sender, RoutedEventArgs e) { }
+    }
+}
