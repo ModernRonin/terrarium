ff79515fe42ed63ce60e8b1fb4c145b4d7a97685
max <7413294+ModernRonin@users.noreply.github.com>
2017-11-18 13:53:40 +0100

    first render implementation

---
+++ Terrarium/prototypes/MonoGameUwpXaml/Vector2DExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+using ModernRonin.Standard;
+
+namespace MonoGameUwpXaml {
+    public static class Vector2DExtensions
+    {
+        public static Point ToPoint(this Vector2D self) => new Point(self.X, self.Y);
+    }
+}
--- Terrarium/prototypes/MonoGameUwpXaml/VisualSimulation.cs
+++ Terrarium/prototypes/MonoGameUwpXaml/VisualSimulation.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using ModernRonin.Standard;
 using ModernRonin.Terrarium.Logic;
 using MoreLinq;
 
@@ -10,9 +11,11 @@ namespace MonoGameUwpXaml
 {
     public class VisualSimulation : Game
     {
+        const int ScalingFactor = 100;
         readonly GraphicsDeviceManager mGraphics;
         readonly Dictionary<PartKind, Texture2D> mPartKindTextures = new Dictionary<PartKind, Texture2D>();
         SpriteBatch mSpriteBatch;
+        readonly Point mScalingVector = new Point(ScalingFactor, ScalingFactor);
         public VisualSimulation()
         {
             mGraphics = new GraphicsDeviceManager(this);
@@ -57,15 +60,32 @@ namespace MonoGameUwpXaml
 
             base.Update(gameTime);
         }
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            mSpriteBatch.Begin();
+            GraphicsDevice.Clear(Color.Black);
+            SimulationState.Entities.ForEach(Draw);
+            mSpriteBatch.End();
+        }
+        void Draw(Entity entity)
+        {
+            void drawSprite(Sprite sprite) => mSpriteBatch.Draw(sprite.Image, sprite.BoundingBox, Color.White);
+            entity.Parts.Select(p => ToSprite(p, entity.Position)).ForEach(drawSprite);
+        }
+        Sprite ToSprite(Part part, Vector2D origin)
+        {
+            var absolutePosition = (origin + part.RelativePosition) * ScalingFactor;
+            return new Sprite()
+            {
+                Image = mPartKindTextures[part.Kind],
+                BoundingBox = new Rectangle(absolutePosition.ToPoint(), mScalingVector)
+            };
+        }
 
-            base.Draw(gameTime);
+        class Sprite
+        {
+            public Rectangle BoundingBox { get; set; }
+            public Texture2D Image { get; set; }
         }
     }
 }
