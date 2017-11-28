83fbd119e428baaa9bca17359f0867cf8e6d7d83
max <7413294+ModernRonin@users.noreply.github.com>
2017-11-28 04:21:23 +0100

    removed rotation

--- Terrarium/prototypes/MonoGameUwpXaml/Camera.cs
+++ Terrarium/prototypes/MonoGameUwpXaml/Camera.cs
@@ -5,13 +5,12 @@ namespace MonoGameUwpXaml
     public class Camera
     {
         Vector2 mPosition;
-        float mRotation;
         float mZoom = 0.05f;
         public int ViewportWidth { get; set; }
         public int ViewportHeight { get; set; }
         Vector2 ViewportCenter => new Vector2(ViewportWidth * 0.5f, ViewportHeight * 0.5f);
         public Matrix TranslationMatrix => Matrix.CreateTranslation(-(int) mPosition.X, -(int) mPosition.Y, 0) *
-                                           Matrix.CreateRotationZ(mRotation) *
+                                           Matrix.CreateRotationZ(0) *
                                            Matrix.CreateScale(new Vector3(mZoom, mZoom, 1)) *
                                            Matrix.CreateTranslation(new Vector3(ViewportCenter, 0));
 
@@ -21,9 +20,6 @@ namespace MonoGameUwpXaml
             if (mZoom < 0.001f) mZoom = 0.001f;
         }
 
-        // Move the camera in an X and Y amount based on the cameraMovement param.
-        // if clampToMap is true the camera will try not to pan outside of the
-        // bounds of the map.
         public void MoveCamera(Vector2 cameraMovement) => mPosition = mPosition + cameraMovement;
         public void CenterOn(Vector2 position) => mPosition = position;
     }
