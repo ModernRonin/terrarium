67e0096ed82f3226fcf225eac8a552975b810626
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-01 00:11:51 +0100

    cleanup

--- Terrarium/ModernRonin.Standard.Tests/Vector2DTests.cs
+++ Terrarium/ModernRonin.Standard.Tests/Vector2DTests.cs
@@ -14,31 +14,6 @@ namespace ModernRonin.Standard.Tests
             underTest.Y.Should().Be(17.2f);
         }
         [Test]
-        public void OperatorPlus_Adds()
-        {
-            var a= new Vector2D(1.1f, 2.2f);
-            var b= new Vector2D(3.3f, 4.4f);
-            var result = a + b;
-            result.X.Should().BeApproximately(4.4f, 0.001f);
-            result.Y.Should().BeApproximately(6.6f, 0.001f);
-        }
-        [Test]
-        public void OperatorMinus_Subtracts()
-        {
-            var a= new Vector2D(1.1f, 2.2f);
-            var b= new Vector2D(3.3f, 4.4f);
-            var result = b-a;
-            result.X.Should().BeApproximately(2.2f, 0.001f);
-            result.Y.Should().BeApproximately(2.2f, 0.001f);
-        }
-        [Test]
-        public void OperatorBy_WithScalar_Multiplies()
-        {
-            var result= new Vector2D(1f, 2f)*3f;
-            result.X.Should().BeApproximately(3f, 0.001f);
-            result.Y.Should().BeApproximately(6f, 0.001f);
-        }
-        [Test]
         public void OperatorBy_Gives_The_Same_Result_No_Matter_Which_Side_The_Scalar_Is_On()
         {
             var scalarLeft = 3f * new Vector2D(1f, 2f);
@@ -47,11 +22,36 @@ namespace ModernRonin.Standard.Tests
             scalarLeft.Y.Should().BeApproximately(scalarRight.Y, 0.001f);
         }
         [Test]
+        public void OperatorBy_WithScalar_Multiplies()
+        {
+            var result = new Vector2D(1f, 2f) * 3f;
+            result.X.Should().BeApproximately(3f, 0.001f);
+            result.Y.Should().BeApproximately(6f, 0.001f);
+        }
+        [Test]
         public void OperatorDivide_Divides()
         {
-            var result= new Vector2D(4f, 8f)/2f;
+            var result = new Vector2D(4f, 8f) / 2f;
             result.X.Should().BeApproximately(2f, 0.001f);
             result.Y.Should().BeApproximately(4f, 0.001f);
+        }
+        [Test]
+        public void OperatorMinus_Subtracts()
+        {
+            var a = new Vector2D(1.1f, 2.2f);
+            var b = new Vector2D(3.3f, 4.4f);
+            var result = b - a;
+            result.X.Should().BeApproximately(2.2f, 0.001f);
+            result.Y.Should().BeApproximately(2.2f, 0.001f);
+        }
+        [Test]
+        public void OperatorPlus_Adds()
+        {
+            var a = new Vector2D(1.1f, 2.2f);
+            var b = new Vector2D(3.3f, 4.4f);
+            var result = a + b;
+            result.X.Should().BeApproximately(4.4f, 0.001f);
+            result.Y.Should().BeApproximately(6.6f, 0.001f);
         }
     }
 }
