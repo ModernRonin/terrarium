d4b9b82358e2f16d8a1e5c8d79b8f9d925b5b9f2
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-01 00:16:05 +0100

    * test project for MR.Terrarium.Logic * first test for EnergySource

---
+++ Terrarium/ModernRonin.Terrarium.Logic.Tests/EnergySourceTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using ModernRonin.Standard;
+using NUnit.Framework;
+
+namespace ModernRonin.Terrarium.Logic.Tests
+{
+    [TestFixture]
+    public class EnergySourceTests
+    {
+        [Test]
+        public void Construction()
+        {
+            var underTest = new EnergySource(new Vector2D(2.3f, 3.1f), 17f);
+            underTest.Position.X.Should().BeApproximately(2.3f, 0.001f);
+            underTest.Position.Y.Should().BeApproximately(3.1f, 0.001f);
+            underTest.Intensity.Should().BeApproximately(17f, 0.001f);
+        }
+    }
+}
