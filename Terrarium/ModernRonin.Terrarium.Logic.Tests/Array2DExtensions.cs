15f8725f0d16df41afd1f1e93b0062791969462f
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-01 01:27:31 +0100

    added extension methods for dealing with 2d arrays

---
+++ Terrarium/ModernRonin.Terrarium.Logic.Tests/Array2DExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+
+namespace ModernRonin.Terrarium.Logic.Tests
+{
+    public static class Array2DExtensions
+    {
+        public static void Set<T>(this T[,] self, T value)
+        {
+            Do(self, (x, y) => self[x, y] = value);
+        }
+        public static void Do<T>(this T[,] self, Action<int, int> what)
+        {
+            for (var x = 0; x < self.GetLength(0); ++x) for (var y = 0; y < self.GetLength(1); ++y) what(x, y);
+        }
+        public static IEnumerable<T> ToEnumerable<T>(this T[,] self)
+        {
+            for (var x = 0; x < self.GetLength(0); ++x)
+            for (var y = 0; y < self.GetLength(1); ++y) yield return self[x, y];
+        }
+        public static void ShouldAllBe<T>(this T[,] self, T expected)
+        {
+            foreach (var element in self.ToEnumerable()) element.Should().Be(expected);
+        }
+    }
+}
--- Terrarium/ModernRonin.Terrarium.Logic.Tests/EnergySourceTests.cs
+++ Terrarium/ModernRonin.Terrarium.Logic.Tests/EnergySourceTests.cs
@@ -1,5 +1,4 @@
-﻿using FluentAssertions;
-using ModernRonin.Standard;
+﻿using ModernRonin.Standard;
 using ModernRonin.Standard.Tests;
 using NUnit.Framework;
 
@@ -11,11 +10,11 @@ namespace ModernRonin.Terrarium.Logic.Tests
         [Test]
         public void ApplyTo_Adds_Full_Intensity_At_Position()
         {
-            var underTest = new EnergySource(new Vector2D(50f, 50f), 100f);
+            var underTest = new EnergySource(new Vector2D(50f, 50f), 10f);
             var grid = new float[100, 100];
-            for (var x= 0; x<grid.GetLength(0); ++x) for (var y = 0; y < grid.GetLength(1); ++y) grid[x, y] = 1f;
+            grid.Set(1f);
             var output = underTest.ApplyTo(grid);
-            AdditionalAssertionExtensions.Should(output[50, 50]).Approximate(101f);
+            output[50, 50].Should().Approximate(11f);
         }
         [Test]
         public void ApplyTo_DoesNot_Change_Input()
@@ -23,13 +22,13 @@ namespace ModernRonin.Terrarium.Logic.Tests
             var underTest = new EnergySource(new Vector2D(50f, 50f), 100f);
             var grid = new float[100, 100];
             underTest.ApplyTo(grid);
-            grid.ShouldBeEquivalentTo(new float[100, 100]);
+            grid.ShouldAllBe(0f);
         }
         [Test]
         public void At_Returns_New_Instance_With_Equal_Intensity()
         {
             var underTest = new EnergySource(new Vector2D(), 13f);
-            AdditionalAssertionExtensions.Should(underTest.At(new Vector2D(1f, 1f)).Intensity).Be(13f);
+            underTest.At(new Vector2D(1f, 1f)).Intensity.Should().Be(13f);
         }
         [Test]
         public void At_Returns_New_Instance_With_Passed_Position()
@@ -42,7 +41,7 @@ namespace ModernRonin.Terrarium.Logic.Tests
         {
             var underTest = new EnergySource(new Vector2D(2.3f, 3.1f), 17f);
             underTest.Position.Should().Approximate(2.3f, 3.1f);
-            AdditionalAssertionExtensions.Should(underTest.Intensity).Approximate(17f);
+            underTest.Intensity.Should().Approximate(17f);
         }
         [Test]
         public void WithIntensity_Returns_New_Instance_With_Equal_Position()
@@ -54,7 +53,7 @@ namespace ModernRonin.Terrarium.Logic.Tests
         public void WithIntensity_Returns_New_Instance_With_Passed_Intensity()
         {
             var underTest = new EnergySource(new Vector2D(), 13f);
-            AdditionalAssertionExtensions.Should(underTest.WithIntensity(17f).Intensity).Be(17f);
+            underTest.WithIntensity(17f).Intensity.Should().Be(17f);
         }
     }
 }
