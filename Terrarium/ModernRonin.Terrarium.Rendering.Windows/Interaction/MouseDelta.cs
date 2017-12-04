9f2d4ae984e66490be788276363a1f04f7f40a30
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-04 04:49:58 +0100

    improved click detection

--- Terrarium/ModernRonin.Terrarium.Rendering.Windows/Interaction/MouseDelta.cs
+++ Terrarium/ModernRonin.Terrarium.Rendering.Windows/Interaction/MouseDelta.cs
@@ -19,15 +19,10 @@ namespace ModernRonin.Terrarium.Rendering.Windows.Interaction
         public bool IsLeftDown { get; private set; }
         public bool IsRightDown { get; private set; }
         public bool IsMiddleDown { get; private set; }
-        public bool HasLeftBeenClicked => IsInClickRange(mLastLeftUp.Subtract(mLastLeftDown));
-        public bool HasRightBeenClicked => IsInClickRange(mLastRightUp.Subtract(mLastRightDown));
-        public bool HasMiddleBeenClicked => IsInClickRange(mLastMiddleUp.Subtract(mLastMiddleDown));
+        public bool HasLeftBeenClicked { get; private set; }
+        public bool HasRightBeenClicked { get; private set; }
+        public bool HasMiddleBeenClicked { get; private set; }
         public Vector2 PointerPosition { get; private set; }
-        bool IsInClickRange(TimeSpan delta)
-        {
-            if (delta.TotalMilliseconds < 0) return false;
-            return delta.TotalMilliseconds < 1000;
-        }
         public void Update()
         {
             var current = Mouse.GetState();
@@ -45,10 +40,41 @@ namespace ModernRonin.Terrarium.Rendering.Windows.Interaction
             if (current.MiddleButton == ButtonState.Released) mLastMiddleUp = now;
             else mLastMiddleDown = now;
 
+            UpdateClicks();
+
             IsLeftDown = current.LeftButton == ButtonState.Pressed;
             IsRightDown = current.RightButton == ButtonState.Pressed;
             IsMiddleDown = current.MiddleButton == ButtonState.Pressed;
             mLast = current;
+        }
+        bool IsInClickRange(TimeSpan delta)
+        {
+            if (delta.TotalMilliseconds < 0) return false;
+            return delta.TotalMilliseconds < 1000;
+        }
+        void UpdateClicks()
+        {
+            HasLeftBeenClicked = false;
+            HasRightBeenClicked = false;
+            HasMiddleBeenClicked = false;
+            HasLeftBeenClicked = IsInClickRange(mLastLeftUp.Subtract(mLastLeftDown));
+            HasRightBeenClicked = IsInClickRange(mLastRightUp.Subtract(mLastRightDown));
+            HasMiddleBeenClicked = IsInClickRange(mLastMiddleUp.Subtract(mLastMiddleDown));
+            if (HasLeftBeenClicked)
+            {
+                mLastLeftDown = DateTime.MinValue;
+                mLastLeftUp = DateTime.MinValue;
+            }
+            if (HasMiddleBeenClicked)
+            {
+                mLastMiddleDown = DateTime.MinValue;
+                mLastMiddleUp = DateTime.MinValue;
+            }
+            if (HasRightBeenClicked)
+            {
+                mLastRightDown = DateTime.MinValue;
+                mLastRightUp = DateTime.MinValue;
+            }
         }
     }
 }
