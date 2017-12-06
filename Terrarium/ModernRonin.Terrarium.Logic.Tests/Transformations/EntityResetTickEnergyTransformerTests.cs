8913014f52244eb35c7c89c4d57afaea05213f56
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-06 09:13:30 +0100

    EntityResetTickEnergyTransformer

---
+++ Terrarium/ModernRonin.Terrarium.Logic.Tests/Transformations/EntityResetTickEnergyTransformerTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using FluentAssertions;
+using ModernRonin.Standard;
+using ModernRonin.Terrarium.Logic.Objects;
+using ModernRonin.Terrarium.Logic.Objects.Entities;
+using ModernRonin.Terrarium.Logic.Transformations;
+using NUnit.Framework;
+
+namespace ModernRonin.Terrarium.Logic.Tests.Transformations
+{
+    [TestFixture]
+    public class EntityResetTickEnergyTransformerTests
+    {
+        [Test]
+        public void Priority_Is_IntMin()
+        {
+            new EntityResetTickEnergyTransformer().Priority.Should().Be(int.MinValue);
+        }
+        [Test]
+        public void Sets_Entities_TickEnergy_To_Zero()
+        {
+            var underTest = new EntityResetTickEnergyTransformer();
+            var entity = new Entity(new EntityState(Null.Enumerable<Part>(), tickEnergy: 14f), null);
+            var state = new SimulationState(new[] {entity}, Null.Enumerable<IEnergySource>());
+
+            var changed = underTest.Transform(state).Entities.Single();
+            changed.State.TickEnergy.Should().Be(0);
+        }
+    }
+}
---
+++ Terrarium/ModernRonin.Terrarium.Logic/Transformations/EntityResetTickEnergyTransformer.cs
@@ -0,0 +1,11 @@
+﻿using ModernRonin.Terrarium.Logic.Objects.Entities;
+
+namespace ModernRonin.Terrarium.Logic.Transformations
+{
+    public class EntityResetTickEnergyTransformer : AnEntityTransformer
+    {
+        public override int Priority => int.MinValue;
+        protected override Entity Transform(Entity old, ISimulationState state) =>
+            old.WithState(old.State.ResetTickEnergy());
+    }
+}
