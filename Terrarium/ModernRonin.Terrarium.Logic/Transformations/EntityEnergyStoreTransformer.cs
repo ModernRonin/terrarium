ac9b26575214da67b45fdb01beaa020d03178e07
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-08 00:11:02 +0100

    implementation of EntityEnergyStoreTransformer, WIP

--- Terrarium/ModernRonin.Terrarium.Logic/Transformations/EntityEnergyStoreTransformer.cs
+++ Terrarium/ModernRonin.Terrarium.Logic/Transformations/EntityEnergyStoreTransformer.cs
@@ -1,4 +1,7 @@
-﻿using ModernRonin.Terrarium.Logic.Config;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ModernRonin.Terrarium.Logic.Config;
 using ModernRonin.Terrarium.Logic.Objects.Entities;
 
 namespace ModernRonin.Terrarium.Logic.Transformations
@@ -8,10 +11,25 @@ namespace ModernRonin.Terrarium.Logic.Transformations
         readonly IPartPropertiesConfiguration mConfiguration;
         public EntityEnergyStoreTransformer(IPartPropertiesConfiguration configuration) =>
             mConfiguration = configuration;
+        protected override IEnumerable<Type> AdditionalDependencies
+        {
+            get
+            {
+                yield return typeof(EntityEnergyAbsorptionTransformer);
+                yield return typeof(EntityCurrentInstructionCostTransformer);
+                yield return typeof(EntityPartsEnergyCostTransformer);
+                
+            }
+        }
         protected override Entity Transform(Entity old, ISimulationState state)
         {
-            var toBeStored = 0;
-            return old.WithState(old.State.AddStoredEnergy(toBeStored));
+            if (old.State.TickEnergy > 0)
+            {
+                var remainingStorageCapacity = old.State.Parts.Where(p => p.Kind==PartKind.Absorber);
+                var toBeStored = 0;
+                return old.WithState(old.State.AddStoredEnergy(toBeStored));
+            }
+            return old;
         }
     }
 }
