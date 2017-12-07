93c257e04f9ef473712c7b5064f95c5f89e75e88
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-08 00:13:42 +0100

    renamed

--- Terrarium/ModernRonin.Terrarium.Logic/Utilities/ModelExtensions.cs
+++ Terrarium/ModernRonin.Terrarium.Logic/Utilities/ModelExtensions.cs
@@ -2,7 +2,7 @@
 
 namespace ModernRonin.Terrarium.Logic.Utilities
 {
-    public static class Vector2DExtensions
+    public static class ModelExtensions
     {
         public static float[,] ToEnergyDensity(this Vector2D self) => new float[(int) self.X, (int) self.Y];
     }
