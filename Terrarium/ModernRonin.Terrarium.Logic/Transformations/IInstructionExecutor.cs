4c68d73e565de6803dfc7275428215efb52c836f
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-08 15:36:49 +0100

    sketch of execution subsystem

--- Terrarium/ModernRonin.Terrarium.Logic/Transformations/IInstructionExecutor.cs
+++ Terrarium/ModernRonin.Terrarium.Logic/Transformations/IInstructionExecutor.cs
@@ -1,4 +1,7 @@
-﻿using ModernRonin.Terrarium.Logic.Objects.Entities;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ModernRonin.Terrarium.Logic.Objects.Entities;
 using ModernRonin.Terrarium.Logic.Objects.Entities.Instructions;
 
 namespace ModernRonin.Terrarium.Logic.Transformations
@@ -6,5 +9,34 @@ namespace ModernRonin.Terrarium.Logic.Transformations
     public interface IInstructionExecutor
     {
         IEntity Execute(IInstruction instruction, IEntity entity, ISimulationState simulationState);
+    }
+
+    public interface ISpecificInstructionExecutor : IInstructionExecutor
+    {
+        Type HandledInstructionType { get; }
+    }
+
+    public abstract class ASpecificInstructionExecutor<T> : ISpecificInstructionExecutor where T:IInstruction
+    {
+        public IEntity Execute(IInstruction instruction, IEntity entity, ISimulationState simulationState)
+        {
+            return DoExecute((T) instruction, entity, simulationState);
+        }
+        public Type HandledInstructionType => typeof(T);
+        protected abstract IEntity DoExecute(T instruction, IEntity entity, ISimulationState simulationState);
+    }
+    public class InstructionExecutor : IInstructionExecutor
+    {
+        readonly Dictionary<Type, ISpecificInstructionExecutor> mSpecificExecutors;
+        public InstructionExecutor(IEnumerable<ISpecificInstructionExecutor> specificExecutors)
+        {
+            mSpecificExecutors = specificExecutors.ToDictionary(e => e.HandledInstructionType);
+        }
+        public IEntity Execute(IInstruction instruction, IEntity entity, ISimulationState simulationState)
+        {
+            if (!mSpecificExecutors.ContainsKey(instruction.GetType()))
+                throw new NotImplementedException();
+            return mSpecificExecutors[instruction.GetType()].Execute(instruction, entity, simulationState);
+        }
     }
 }
