5a8988a2e5cb5361dd0d857756483072b47a9673
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-08 01:56:35 +0100

    RemoveEntitiesWithNegativeTickEnergyTransformer implemented and tested

--- Terrarium/ModernRonin.Terrarium.Logic.Tests/Transformations/RemoveEntitiesWithNegativeTickEnergyTransformerTests.cs
+++ Terrarium/ModernRonin.Terrarium.Logic.Tests/Transformations/RemoveEntitiesWithNegativeTickEnergyTransformerTests.cs
@@ -1,4 +1,8 @@
-﻿using NSubstitute;
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using ModernRonin.Terrarium.Logic.Objects.Entities;
+using ModernRonin.Terrarium.Logic.Transformations;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace ModernRonin.Terrarium.Logic.Tests.Transformations
@@ -9,9 +13,23 @@ namespace ModernRonin.Terrarium.Logic.Tests.Transformations
         [Test]
         public void Removes_Entities_With_Negative_TickEnergy()
         {
+            var alpha = Substitute.For<IEntity>();
+            var bravo = Substitute.For<IEntity>();
+            var charlie = Substitute.For<IEntity>();
+            alpha.State.TickEnergy.Returns(11);
+            bravo.State.TickEnergy.Returns(-3);
+            charlie.State.TickEnergy.Returns(0);
             var state = Substitute.For<ISimulationState>();
-//            var alpha= new Entity
+            state.Entities.Returns(new[] {alpha, bravo, charlie});
+            state.WhenForAnyArgs(s => s.WithEntities(null)).Do(ci =>
+            {
+                ci.Arg<IEnumerable<IEntity>>().Should().OnlyContain(e => e == alpha || e == charlie);
+            });
+
+            var underTest = new RemoveEntitiesWithNegativeTickEnergyTransformer();
+            underTest.Transform(state);
+
+            state.ReceivedWithAnyArgs().WithEntities(null);
         }
     }
 }
-
