dfe7b97363b19756767965f88a2f42628132dd70
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-08 11:38:54 +0100

    * EntityExecuteCurrentInstructionTransformer skeleton * testing that executing the current instruction of entities comes after removing the dead ones

--- Terrarium/ModernRonin.Terrarium.Logic.Tests/Transformations/TransformerDependenciesTests.cs
+++ Terrarium/ModernRonin.Terrarium.Logic.Tests/Transformations/TransformerDependenciesTests.cs
@@ -23,6 +23,7 @@ namespace ModernRonin.Terrarium.Logic.Tests.Transformations
                 new EntityPartsEnergyCostTransformer(null),
                 new EntityResetTickEnergyTransformer(),
                 new RemoveEntitiesWithNegativeTickEnergyTransformer(),
+                new EntityExecuteCurrentInstructionTransformer(), 
             }.ToDictionary(t => t.GetType());
         static IEnumerable<Dependency> TestCases
         {
@@ -38,6 +39,8 @@ namespace ModernRonin.Terrarium.Logic.Tests.Transformations
                 yield return DependencyOf<EntityEnergyStoreTransformer>().On<EntityPartsEnergyCostTransformer>();
                 yield return DependencyOf<EntityPartsEnergyCostTransformer>().On<EntityResetTickEnergyTransformer>();
                 yield return DependencyOf<RemoveEntitiesWithNegativeTickEnergyTransformer>().On<EntityEnergyStoreTransformer>();
+                yield return DependencyOf<EntityExecuteCurrentInstructionTransformer>()
+                    .On<RemoveEntitiesWithNegativeTickEnergyTransformer>();
             }
         }
 
---
+++ Terrarium/ModernRonin.Terrarium.Logic/Transformations/EntityExecuteCurrentInstructionTransformer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using ModernRonin.Terrarium.Logic.Objects.Entities;
+using ModernRonin.Terrarium.Logic.Transformations.Framework;
+
+namespace ModernRonin.Terrarium.Logic.Transformations
+{
+    public class EntityExecuteCurrentInstructionTransformer : AnEntityTransformer
+    {
+        public override IEnumerable<Type> Dependencies
+        {
+            get { yield return typeof(RemoveEntitiesWithNegativeTickEnergyTransformer); }
+        }
+        protected override IEntity Transform(IEntity old, ISimulationState state) =>
+            throw new NotImplementedException();
+    }
+}
