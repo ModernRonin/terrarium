87b1f600ae3f72336a8826c8ff247a934d83b616
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-09 17:29:49 +0100

    .ReplaceEntity() extension method

---
+++ Terrarium/ModernRonin.Terrarium.Logic.Tests/Utilities/ModelExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using ModernRonin.Terrarium.Logic.Objects.Entities;
+using ModernRonin.Terrarium.Logic.Utilities;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ModernRonin.Terrarium.Logic.Tests.Utilities
+{
+    [TestFixture]
+    public class ModelExtensionsTests
+    {
+        [Test]
+        public void ReplaceEntity_Returns_State_With_Changed_Entities()
+        {
+            var alpha = Substitute.For<IEntity>();
+            var bravo = Substitute.For<IEntity>();
+            var entities = new[]
+                {Substitute.For<IEntity>(), Substitute.For<IEntity>(), alpha, Substitute.For<IEntity>()};
+            var state = Substitute.For<ISimulationState>();
+            state.Entities.Returns(entities);
+            var newState = Substitute.For<ISimulationState>();
+            IEnumerable<IEntity> newEntities = null;
+            state.WithEntities(Arg.Do<IEnumerable<IEntity>>(e => newEntities = e)).Returns(newState);
+
+            state.ReplaceEntity(alpha, bravo).Should().BeSameAs(newState);
+            newEntities.Should().HaveCount(4);
+            newEntities.Should().Contain(bravo);
+            newEntities.Should().Contain(entities[0]);
+            newEntities.Should().Contain(entities[1]);
+            newEntities.Should().Contain(entities[3]);
+        }
+    }
+}
--- Terrarium/ModernRonin.Terrarium.Logic/Utilities/ModelExtensions.cs
+++ Terrarium/ModernRonin.Terrarium.Logic/Utilities/ModelExtensions.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using ModernRonin.Standard;
 using ModernRonin.Terrarium.Logic.Objects.Entities;
+using MoreLinq;
 
 namespace ModernRonin.Terrarium.Logic.Utilities
 {
@@ -10,5 +11,10 @@ namespace ModernRonin.Terrarium.Logic.Utilities
         public static float[,] ToEnergyDensity(this Vector2D self) => new float[(int) self.X, (int) self.Y];
         public static IEnumerable<Part> OfKind(this IEnumerable<Part> self, PartKind kind) =>
             self.Where(p => p.Kind == kind);
+        public static ISimulationState ReplaceEntity(this ISimulationState self, IEntity original, IEntity replacement)
+        {
+            var entities = replacement.Concat(self.Entities.Except(original.AsEnumerable()));
+            return self.WithEntities(entities);
+        }
     }
 }
