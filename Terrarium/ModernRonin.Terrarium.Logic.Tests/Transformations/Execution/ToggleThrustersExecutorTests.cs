e989dd3851d42abdecf6034b7568b97885344da2
max <7413294+ModernRonin@users.noreply.github.com>
2017-12-09 17:57:35 +0100

    test coverage for ToggleThrustersExecutor

---
+++ Terrarium/ModernRonin.Terrarium.Logic.Tests/Transformations/Execution/ToggleThrustersExecutorTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using FluentAssertions;
+using ModernRonin.Standard;
+using ModernRonin.Terrarium.Logic.Objects;
+using ModernRonin.Terrarium.Logic.Objects.Entities;
+using ModernRonin.Terrarium.Logic.Objects.Entities.Instructions;
+using ModernRonin.Terrarium.Logic.Transformations.Execution;
+using NUnit.Framework;
+
+namespace ModernRonin.Terrarium.Logic.Tests.Transformations.Execution
+{
+    [TestFixture]
+    public class ToggleThrustersExecutorTests
+    {
+        [Test]
+        public void Handles_ToggleThrustersInstruction()
+        {
+            new ToggleThrustersExecutor().HandledInstructionType.Should().Be<ToggleThrustersInstruction>();
+        }
+        [Test]
+        public void Sets_Thrusters_To_Off_If_They_Were_On()
+        {
+            var instruction = new ToggleThrustersInstruction();
+            var entity = new Entity(new EntityState(Null.Enumerable<Part>(), areThrustersOn: true),
+                new Genome(null, new[] {instruction}));
+            var simulationState = new SimulationState(entity.AsEnumerable(), Null.Enumerable<IEnergySource>());
+
+            var underTest = new ToggleThrustersExecutor();
+            underTest.Execute(instruction, entity, simulationState).Entities.Single().State.AreThrustersOn.Should()
+                     .BeFalse();
+        }
+        [Test]
+        public void Sets_Thrusters_To_On_If_They_Were_Off()
+        {
+            var instruction = new ToggleThrustersInstruction();
+            var entity = new Entity(new EntityState(Null.Enumerable<Part>()), new Genome(null, new[] {instruction}));
+            var simulationState = new SimulationState(entity.AsEnumerable(), Null.Enumerable<IEnergySource>());
+
+            var underTest = new ToggleThrustersExecutor();
+            underTest.Execute(instruction, entity, simulationState).Entities.Single().State.AreThrustersOn.Should()
+                     .BeTrue();
+        }
+    }
+}
